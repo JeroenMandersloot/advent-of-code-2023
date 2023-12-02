993a183109f7deef11e0d25744657f2e2839b867
Jeroen Mandersloot <>
2023-12-02 10:50:07 +0100

    Cleanup

--- day02.py
+++ day02.py
@@ -1,27 +1,25 @@
-import contextlib
 import re
 
 from utils import get_input
 
+LIMITS = {"red": 12, "green": 13, "blue": 14}
 
-def is_possible(line, red, green, blue):
-    colors = {"red": red, "green": green, "blue": blue}
-    for color, limit in colors.items():
-        with contextlib.suppress(ValueError):
-            if max(map(int, re.findall(fr"\d+(?= {color})", line))) > limit:
-                return None
+
+def is_possible(line):
+    for color, limit in LIMITS.items():
+        if max(map(int, re.findall(fr"\d+(?= {color})", line))) > limit:
+            return None
     return int(re.search(r"^Game (\d+):", line).group(1))
 
 
 def get_power(line):
-    colors = ["red", "green", "blue"]
     res = 1
-    for color in colors:
+    for color in ("red", "green", "blue"):
         res *= max(map(int, re.findall(fr"\d+(?= {color})", line)))
     return res
 
 
 if __name__ == '__main__':
     lines = get_input(2).split("\n")
-    print(sum(filter(None, (is_possible(line, 12, 13, 14) for line in lines))))
-    print(sum(map(get_power, lines)))
+    print(sum(filter(None, map(is_possible, lines))))  # Part 1
+    print(sum(map(get_power, lines)))  # Part 2
