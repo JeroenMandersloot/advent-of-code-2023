5b13dfb9137218b82f17c9a4ded8f4c11c01de60
Jeroen Mandersloot <>
2023-12-02 10:57:47 +0100

    Remove filter

--- day02.py
+++ day02.py
@@ -8,7 +8,7 @@ LIMITS = {"red": 12, "green": 13, "blue": 14}
 def is_possible(line):
     for color, limit in LIMITS.items():
         if max(map(int, re.findall(fr"\d+(?= {color})", line))) > limit:
-            return None
+            return 0
     return int(re.search(r"^Game (\d+):", line).group(1))
 
 
@@ -21,5 +21,5 @@ def get_power(line):
 
 if __name__ == '__main__':
     lines = get_input(2).split("\n")
-    print(sum(filter(None, map(is_possible, lines))))  # Part 1
+    print(sum(map(is_possible, lines)))  # Part 1
     print(sum(map(get_power, lines)))  # Part 2
