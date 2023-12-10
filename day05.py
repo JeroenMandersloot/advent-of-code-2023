0e713b5fcb786797be8c4c7d91c7d028fe825261
Jeroen Mandersloot <>
2023-12-10 02:03:30 +0100

    Day 5

--- day05.py
+++ day05.py
@@ -16,7 +16,6 @@ def parse(text):
                 dst_start, src_start, length = map(int, line.split(" "))
                 mapping[range(src_start, src_start + length)] = dst_start
             mappings.append(mapping)
-    mappings.append(mapping)
     return seeds, mappings
 
 
@@ -29,42 +28,24 @@ def get_location(seed, mappings):
     return seed
 
 
-def kjk(r, mapping):
+def split_range(r, mapping):
     xs = iter(sorted(mapping, key=lambda x: x.start))
     x = next(xs)
     start = r.start
-    while start < r.stop:
-        if x and start < x.start:
+    while x and start < r.stop:
+        if start < x.start:
             yield range(start, x.start)
             start = x.start
 
-        if x:
-            stop = min(r.stop, x.stop)
-            jump = mapping[x] - x.start
-        else:
-            stop = r.stop
-            jump = 0
-
+        stop = min(r.stop, x.stop)
         if stop > start:
-            if start + jump == 0:
-                print(start + jump, start, jump, x, mapping[x], r)
+            jump = mapping[x] - x.start
             yield range(start + jump, stop + jump)
             start = stop
-
         x = next(xs, None)
 
-
-def asahfjhasjfhd(rs, mappings):
-    for mapping in mappings:
-        print(rs)
-        print(mapping)
-        rs = sorted(
-            chain.from_iterable(kjk(r, mapping) for r in rs),
-            key=lambda x: x.start
-        )
-        print(rs)
-        print("---")
-    return list(rs)
+    if start < r.stop:
+        yield range(start, r.stop)
 
 
 def pairwise(x):
@@ -78,12 +59,14 @@ def part1(seeds, mappings):
 
 
 def part2(seeds, mappings):
-    seeds = [range(a, a + b) for a, b in pairwise(seeds)]
-    return min(r.start for r in asahfjhasjfhd(seeds, mappings))
+    rs = (range(a, a + b) for a, b in pairwise(seeds))
+    for mapping in mappings:
+        rs = list(chain.from_iterable(split_range(r, mapping) for r in rs))
+    return min(r.start for r in rs)
 
 
 if __name__ == '__main__':
-    text = get_input(5, example=False)
+    text = get_input(5)
     seeds, mappings = parse(text)
     print(part1(seeds, mappings))
     print(part2(seeds, mappings))
