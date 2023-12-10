1cf403ff829894927f05529b98d8fb4a172cb6e6
Jeroen Mandersloot <>
2023-12-10 16:19:38 +0100

    Ugly solution for day 10, but hey, it works

---
+++ day10.py
@@ -0,0 +1,130 @@
+from itertools import product
+
+from utils import BaseGrid, get_input
+
+
+class Grid(BaseGrid):
+    def get_connections(self, x, y):
+        for d in self.get_openings(x, y):
+            if self.is_connected(x, y, d):
+                yield self.get_neighbour(x, y, d)
+
+    def is_connected(self, x, y, d):
+        try:
+            xn, yn = self.get_neighbour(x, y, d)
+            opposite = self.D4[(self.D4.index(d) + 2) % 4]
+            return opposite in self.get_openings(xn, yn)
+        except IndexError:
+            return False
+
+    def get_openings(self, x, y):
+        c = self.grid[y][x]
+        if c in "SJL|┃┗┛": yield "N"  # North
+        if c in "SFL-━┗┏": yield "E"  # East
+        if c in "SF7|┃┓┏": yield "S"  # South
+        if c in "SJ7-━┓┛": yield "W"  # West
+
+
+def find_path(grid):
+    s = grid.search("S")
+    grid.set_position(s)
+    path = [grid.pos]
+    seen = set(path)
+    prev = None
+    while s not in (ps := set(grid.get_connections(*grid.pos))) or prev == s:
+        p = next(p for p in ps if p not in seen)
+        prev = grid.pos
+        grid.set_position(*p)
+        path.append(p)
+        seen.add(p)
+    return path
+
+
+def part1(grid):
+    path = find_path(grid)
+    return len(path) // 2
+
+
+def part2(grid):
+    path = find_path(grid)
+    rpath = {v: k for k, v in enumerate(path)}
+    loop = set(path)
+    seen = set(path)
+    options = set(product(range(grid.width), range(grid.height))) - seen
+    inside = set()
+    outside = set()
+    while options:
+        is_inside = True
+        queue = [next(iter(sorted(options)))]
+        slips = set()
+        area = set()
+        while queue:
+            c = queue.pop(0)
+            if c == (27, 20):
+                print(grid[c])
+            if c in area or c in seen:
+                continue
+            if grid.is_border(*c):
+                is_inside = False
+            area.add(c)
+            neighbours = set(grid.get_neighbours(*c, diagonal=True))
+            queue += neighbours - seen - area
+            slip_candidates = {
+                                  frozenset({a, b})
+                                  for a in neighbours.intersection(loop)
+                                  for b in
+                                  set(grid.get_neighbours(*a)).intersection(
+                                      loop).intersection(neighbours)
+                                  if path[rpath[a] - 1] != b and path[
+                    rpath[b] - 1] != a
+                              } - slips
+
+            while slip_candidates:
+                slip_queue = [next(iter(slip_candidates))]
+                while slip_queue:
+                    slip = slip_queue.pop(0)
+                    if slip in slips:
+                        continue
+                    if slip == frozenset({(25, 18), (26, 18)}):
+                        print(slip)
+                    slips.add(slip)
+                    a, b = slip
+                    a_neighbours = set(grid.get_neighbours(*a, diagonal=True))
+                    b_neighbours = set(grid.get_neighbours(*b, diagonal=True))
+                    joint_neighbours = a_neighbours.intersection(b_neighbours)
+                    if joint_neighbours.intersection(outside):
+                        is_inside = False
+                    queue += joint_neighbours - seen - area
+
+                    slip_continuations = set()
+                    for j in joint_neighbours.intersection(loop):
+                        if path[rpath[j] - 1] != b and path[rpath[b] - 1] != j:
+                            slip_continuations.add(frozenset({j, b}))
+                        if path[rpath[j] - 1] != a and path[rpath[a] - 1] != j:
+                            slip_continuations.add(frozenset({a, j}))
+
+                    slip_queue += slip_continuations - slips
+                slip_candidates -= slips
+
+        if is_inside:
+            inside |= area
+        else:
+            outside |= area
+        seen |= area
+        options -= seen
+    return sorted(inside)
+
+
+if __name__ == '__main__':
+    grid = Grid(get_input(10, example=True, lines=True))
+    path = set(find_path(grid))
+    print(part1(grid))
+    inside = part2(grid)
+    for pos in inside:
+        grid[pos] = "I"
+    for pos in product(range(grid.width), range(grid.height)):
+        if pos not in path and pos not in inside:
+            grid[pos] = "O"
+    table = str.maketrans("FJ7L-|", "┏┛┓┗━┃")
+    print(str(grid).translate(table))
+    print(len(inside))
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import contextlib
 import os
 from typing import Sequence, Union
 
@@ -13,3 +14,89 @@ def get_input(
         text = f.read()
 
     return text.strip("\n").split("\n") if lines else text
+
+
+class BaseGrid:
+    D4 = "NESW"
+    D8 = ["N", "E", "S", "W", "NE", "SE", "SW", "NW"]
+
+    def __init__(self, grid, start=None, marker=None):
+        self.grid = grid
+        self.marker = marker
+        self.pos = start
+
+    def move(self, d, skip_errors=False):
+        if self.pos is None:
+            raise ValueError("No position")
+        x, y = self.pos
+        try:
+            self.pos = self.get_neighbour(x, y, d)
+        except IndexError:
+            if not skip_errors:
+                raise
+
+    def __getitem__(self, pos):
+        x, y = pos
+        return self.grid[y][x]
+
+    def __setitem__(self, pos, value):
+        x, y = pos
+        line = list(self.grid[y])
+        line[x] = value
+        self.grid[y] = "".join(line)
+
+    def search(self, c):
+        for y, line in enumerate(self.grid):
+            with contextlib.suppress(ValueError):
+                return line.index(c), y
+        raise ValueError(f"Character {c} not found")
+
+    def set_position(self, x, y=None):
+        if y is None:
+            x, y = x
+        self.pos = (x, y)
+
+    @property
+    def width(self):
+        return max(map(len, self.grid))
+
+    @property
+    def height(self):
+        return len(self.grid)
+
+    def is_border(self, x, y):
+        return x == 0 or y == 0 or x == self.width - 1 or y == self.height - 1
+
+    def get_neighbour(self, x, y, d):
+        x, y = {
+            "N": (x, y - 1),
+            "E": (x + 1, y),
+            "S": (x, y + 1),
+            "W": (x - 1, y),
+            "NE": (x + 1, y - 1),
+            "SE": (x + 1, y + 1),
+            "SW": (x - 1, y + 1),
+            "NW": (x - 1, y - 1),
+        }[d]
+        if x < 0 or y < 0 or x >= self.width or y >= self.height:
+            raise IndexError(f"({x}, {y})")
+        return x, y
+
+    def get_neighbours(self, x, y, diagonal=False):
+        ds = ["N", "E", "S", "W"]
+        if diagonal:
+            ds += ["NE", "SE", "SW", "NW"]
+        for d in ds:
+            with contextlib.suppress(IndexError):
+                yield self.get_neighbour(x, y, d)
+
+    def show(self):
+        print(f"{self}\n")
+
+    def __str__(self):
+        if self.pos and self.marker:
+            g = list(map(list, self.grid))
+            x, y = self.pos
+            g[y][x] = self.marker
+            return "\n".join("".join(line) for line in g)
+        return "\n".join(self.grid)
